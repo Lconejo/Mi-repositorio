ae7e78f34447fa5c552133f8c94bde42c1169604
49OscarRamírez <143234112+Lconejo@users.noreply.github.com>
2024-12-30 21:17:21 -0600

    Modifiqué el codigo

--- Saludo.c
+++ Saludo.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(void){
+    int num;
     printf("Hola, Un saludo ando aprendiendo git otra vez jsjskj");
+    printf("Ingresa tu numero favorito:");
+    scanf("%d", &num);
+    printf("Genial tu numero favorito es el %d", num);
     return 0;
 }
