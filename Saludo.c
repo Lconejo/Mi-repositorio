cd2f6c09131cb97697884d25a291da5963d6fc4c
49OscarRamírez <143234112+Lconejo@users.noreply.github.com>
2024-12-30 21:23:16 -0600

    mensaje de despedida

--- Saludo.c
+++ Saludo.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(void){
     int num;
-    printf("Hola, Un saludo ando aprendiendo git otra vez jsjskj");
+    printf("Hola, Un saludo ando aprendiendo git otra vez jsjskj\n\n");
     printf("Ingresa tu numero favorito:");
     scanf("%d", &num);
-    printf("Genial tu numero favorito es el %d", num);
+    printf("\n\nGenial tu numero favorito es el %d", num);
+    printf("byeee :]");
     return 0;
 }
